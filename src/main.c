ceabb84870c9a76694cb6b30b5a8903eadd543f3
Youssef Shawki <ya0721435@gmail.com>
2024-06-20 14:57:08 +0300

    resolve conflict in the main.c

--- src/main.c
+++ src/main.c
@@ -9,31 +9,6 @@ static void delay(uint32 time){
     for ( i = 0 ; i<time;i++){}
 }
 
-<<<<<<< HEAD
-int main(void){
-
-    Rcc_Init();
-    HAL_Init();
-	Rcc_Enable(RCC_GPIOB);
-	Rcc_Enable(RCC_WWDG);
-
-//    initialization.
-    LEDM_Init();
-    WDGDrv_Init();
-    WDGM_Init();
-
-
-    while (1) {
-        // Call LEDM_Manage every 10ms
-//    	delay(10);
-    	HAL_Delay(10);
-        LEDM_Manage();
-
-        // Call WDGM_MainFunction every 20ms
-//        delay(20);
-    	HAL_Delay(20);
-        WDGM_MainFunction();
-=======
 int main(void) {
     // Set PB0 as output for the LED
     DDRB |= (1 << 0);
@@ -45,7 +20,6 @@ int main(void) {
     while (1) {
         // delay(10);
         // LEDM_Manage();
->>>>>>> d2c87f4818c04e781775bf922690406f29322ed3
 
         // // Call WDGM_MainFunction every 20ms
         // delay(20);
