e404ab9cd72313b6634d82ebc6654c36d906d30a
SaraElsaggan <sara.mahmoud.sm600@gmail.com>
2024-06-21 07:52:39 +0300

    led and timer configuration

--- WatchDog/WDGDRV.c
+++ WatchDog/WDGDRV.c
@@ -1,48 +1,69 @@
-// // #include "stm32l0xx.h" 
-// #include "WDGDRV.h"
-// #include "WDGM.h"
-// #include "Std_Types.h"
+// #include "stm32l0xx.h" 
+#include "WDGDRV.h"
+#include "WDGM.h"
+#include "Std_Types.h"
 // #include <util/delay.h>
-// #include <avr/wdt.h>
-// #include <avr/interrupt.h>
+#include <avr/wdt.h>
+#include <avr/interrupt.h>
+#include <avr/io.h>
 
 
 
-// int x = 0;
+
+int x = 0;
 
 
-// ISR(WDT_vect) {
-//     x = 1;
-//     PORTB |= (1 << 0); // Set PB0 high
-//     // for ( volatile uint32 i = 0; i < 10000000000; i++) {/* Delay loop*/}
-//     // _delay_ms(1000); 
-//     // WDTCSR = (1 << WDIE);
-// }
+ISR(TIMER1_COMPA_vect) {
+    x = 1;
+    PORTB |= (1 << 0); // Set PB0 high
+    // wdt_reset(); // Reset the watchdog timer
 
-// void WDGDrv_Init(void) {
-//     DDRB |= (1 << 0);
-//     PORTB &= ~(1 << 0);
-//     // Disable global interrupts
-//     cli();
-//     // SREG &= ~(1 << I);
-//     //Interrupt and System Reset Mode
-//     wdt_enable(2);
-//     WDTCSR = (1 << WDCE) | (1 << WDE);// Set the Watchdog change enable bit and Watchdog system reset enable bit in one operation
-//     WDTCSR =  (1 << WDE)|(1 << WDIE) | (1 << WDP1);    // Set the prescaler to 64 seconds and enable the Watchdog interrupt
-//     sei(); // Enable global interrupts
-//     // SREG |= (1 << I);
-// // 
-// }
+    // for ( volatile uint32 i = 0; i < 10000000000; i++) {/* Delay loop*/}
+    // _delay_ms(1000); 
+    // WDTCSR = (1 << WDIE);
+}
+
+void WDGDrv_Init(void) {
+
+    cli();
+    // configure the timer
+    TCCR1B |= (1 << WGM12);
+
+    // Set the value for a 50ms interval
+    // Using a prescaler of 64: (16e6 / 64) = 250000 Hz Timer Frequency
+    // 50ms = 0.05s, so OCR1A = 0.05 * 250000 = 12500 - 1 (subtract 1 because it starts from 0)
+    OCR1A = 12499;
+
+    // Set the prescaler to 64
+    TCCR1B |= (1 << CS11) | (1 << CS10);
+
+    // Enable Timer Compare Interrupt
+    TIMSK1 |= (1 << OCIE1A);
 
 
-// void WDGDrv_IsrNotification(void){
-//     // uint32 current_time = HAL_GetTick();
 
-//     if(WDGM_PovideSuppervisionStatus()==OK  /*&& the WDGM_MainFunction_is_not_STUCK*/){
-//        //refresh 
-//     }else{
-//         return;
-//     }
-// }
+    DDRB |= (1 << 0);
+    PORTB &= ~(1 << 0);
+    // Disable global interrupts
+    // SREG &= ~(1 << I);
+    //Interrupt and System Reset Mode
+    // wdt_enable(2);
+    // WDTCSR = (1 << WDCE) | (1 << WDE);// Set the Watchdog change enable bit and Watchdog system reset enable bit in one operation
+    // WDTCSR =  (1 << WDE)| (1 << WDP1);    // Set the prescaler to 64 seconds and enable the Watchdog interrupt
+    sei(); // Enable global interrupts
+    // SREG |= (1 << I);
+// 
+}
+
+
+void WDGDrv_IsrNotification(void){
+    // uint32 current_time = HAL_GetTick();
+
+    if(WDGM_PovideSuppervisionStatus()==OK  /*&& the WDGM_MainFunction_is_not_STUCK*/){
+       //refresh 
+    }else{
+        return;
+    }
+}
 
 
--- src/main.c
+++ src/main.c
@@ -1,40 +1,31 @@
-#include <avr/io.h>
-#include <avr/interrupt.h>
+#include "WDGDRV.h"
+#include <util/delay.h>
+#include <GPIO.h>
+#include <LEDM.h>
+#include <WDGM.h>
 
-void setup() {
-    // Set CTC mode
-    TCCR1A = 0; // Clear Timer/Counter Control Register A
-    TCCR1B = 0; // Clear Timer/Counter Control Register B
-    TCCR1B |= (1 << WGM12); // Set CTC mode (WGM12: Waveform Generation Mode bit 12)
-    
-    // Set the compare match value for 50ms
-    OCR1A = 12499; // Compare match register value (16MHz / (64 * (12499 + 1)) = 50Hz)
-    
-    // Set the prescaler to 64
-    TCCR1B |= (1 << CS11) | (1 << CS10); // CS11 and CS10 bits set to 1: Prescaler = 64
-    
-    // Enable the Timer1 compare match interrupt
-    TIMSK1 |= (1 << OCIE1A); // Output Compare A Match Interrupt Enable
-    
-    // Set PB0 as an output pin
-    DDRB |= (1 << DDB0);
-    
-    // Enable global interrupts
-    sei();
-}
-
-// ISR to be called every 50ms
-ISR(TIMER1_COMPA_vect) {
-    // Toggle the LED on PB0
-    PORTB ^= (1 << PORTB0);
-}
 
 int main(void) {
-    setup(); // Call the setup function
-    
+    // Set PB0 as output for the LED
+	GPIO_Init();
+
+     WDGDrv_Init();
+    LEDM_Init();
+    WDGM_Init();
+
+    int counter = 1;
+    LEDM_Manage();
+    WDGM_MainFunction();
     while (1) {
-        // Main loop does nothing, all work is done in ISR
+         _delay_ms(10);         // Wait for 10 milliseconds
+         LEDM_Manage();
+         if (counter == 2)
+         {
+           WDGM_MainFunction();
+           counter = 1;
+         }
+         counter++;
+
     }
-    
-    return 0; // Although this line will never be reached
+    return 0;
 }
